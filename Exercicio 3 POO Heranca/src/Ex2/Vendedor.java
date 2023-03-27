fafd2d3c6f13a8cf96d7d6e6c3d8c053c76ec00d
Nathalia Suzin <98620648+natsuzin@users.noreply.github.com>
2023-03-27 16:20:22 -0300

    Update Vendedor.java

--- Exercicio 3 POO Heranca/src/Ex2/Vendedor.java
+++ Exercicio 3 POO Heranca/src/Ex2/Vendedor.java
@@ -1,6 +1,39 @@
 package Ex2;
 
-public class Vendedor
+public class Vendedor extends Empregado
 {
-
+  private float percentualComissao;
+  
+  public Empregado()
+  {
+    super();
+    percentualComissao = 0;
+  }
+  
+  public Empregado(String nome, float salario, float percentualComissao)
+  {
+    super(nome, salario);
+    this.percentualComissao = percentualComissao;
+  }
+  
+  public void setPercentualComissao(float percentualComissao)
+  {
+    this.percentualComissao = percentualComissao;
+  }
+  
+  public float getPercentualComissao()
+  {
+    return percentualComissao;
+  }
+  
+  public float calcularSalario()
+  {
+    return super.getSalario() + (super.getSalario() * (getPercentualComisao() / 100));
+  }
+  
+  @Override
+  public String toString()
+  {
+  return String.format(super.toString() + "%nPercentual: %.2f%n Salário com comissão: %.2f%n", getPercentualComisao()/100, calcularSalario());
+  }
 }
